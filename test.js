2c8b7485381c4bc34242300747f67597543eef94
kristijanvlahek <55710982+kristijanvlahek@users.noreply.github.com>
2020-04-06 12:14:12 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+alert('Ovo je poruka koja će se prikazati u prozoru');
+confirm('Molimo odaberite');
+prompt('Molimo unesite koliko Vam je godina', '10');
+document.write('Test ispisivanja na stranicu');
+function funkcija(){
+    document.getElementById("test").innerHTML = 'Odlomak se promijenio';
+}
+console.log('Ova poruka je vidljiva u konzoli');
